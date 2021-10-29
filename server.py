4646211aad8e49d005336cfc9be6b692d51329a1
Mohammad Moinul islam <71957374+momizgit@users.noreply.github.com>
2021-10-30 07:38:22 +1100

    Update server.py
    new code added

--- server.py
+++ server.py
@@ -1,17 +1,44 @@
-from flask import Flask,jsonify,request
+#!/usr/bin/env python3
 
-app = Flask(__name__)
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse
+import json
 
-@app.route('/out', methods = ['GET'])
-def ReturnJSON():
-	if(request.method == 'GET'):
-		data = {
-			"Name" : "Light",
-			"Status" : "ON",
-			"IP" : "1.1.1.1",
-		}
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_path = urlparse(self.path)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(json.dumps({
+            'method': self.command,
+            'path': self.path,
+            'real_path': parsed_path.query,
+            'query': parsed_path.query,
+            'request_version': self.request_version,
+            'protocol_version': self.protocol_version
+        }).encode())
+        return
 
-		return jsonify(data)
+    def do_POST(self):
+        content_len = int(self.headers.getheader('content-length'))
+        post_body = self.rfile.read(content_len)
+        data = json.loads(post_body)
 
-if __name__=='__main__':
-	app.run(debug=True)
+        parsed_path = urlparse(self.path)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(json.dumps({
+            'method': self.command,
+            'path': self.path,
+            'real_path': parsed_path.query,
+            'query': parsed_path.query,
+            'request_version': self.request_version,
+            'protocol_version': self.protocol_version,
+            'body': data
+        }).encode())
+        return
+
+if __name__ == '__main__':
+    server = HTTPServer(('localhost', 8000), RequestHandler)
+    print('Starting server at http://localhost:8000')
+    server.serve_forever()
